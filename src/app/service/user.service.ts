904d5c9c0d74eca32e17d023ce936c5cc8fcb2e5
m.poettgen@gmx.net <82958985+poettas@users.noreply.github.com>
2022-09-11 12:21:05 +0200

    created get-routes on userService

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +9,20 @@ import { Injectable } from '@angular/core';
 export class UserService {
   private readonly urlApi: string = 'https://dummyjson.com/users';
 
-  constructor() {}
+  //using httpClient
+  constructor(private http: HttpClient) {}
+
+  // fetch all users from api
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.urlApi}`);
+  }
+
+  // fetch single user details
+  getOneUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.urlApi}/:${id}`);
+  }
+
+  // edit single user details
+
+  // delete a user from the api
 }
