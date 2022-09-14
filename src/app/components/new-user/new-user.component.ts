7c95726fe9cda70c20ec6035a6ce6385e37f4b86
m.poettgen@gmx.net <82958985+poettas@users.noreply.github.com>
2022-09-14 08:56:04 +0200

    added new User fetch

--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interface/user';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-new-user',
@@ -6,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-user.component.css'],
 })
 export class NewUserComponent implements OnInit {
-  constructor() {}
+  user: User;
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {}
+
+  createUser() {
+    fetch('https://dummyjson.com/users/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        firstName: 'Muhammad',
+        lastName: 'Ovi',
+        username: 'Mu',
+        birthDate: '15.12.1850',
+        email: 'm.ovi@test.com',
+        password: '******',
+        gender: 'male',
+        image: 'https://robohash.org/ovi',
+      }),
+    })
+      .then((res) => res.json())
+      .then(console.log);
+  }
+
+  // createUser() {
+  //   this.userService.createUser(this.user).subscribe(
+  //     (response) => console.log(response),
+  //     (error: any) => console.log(error),
+  //     () => console.log('Done creating new User')
+  //   );
+  // }
 }
